be0c51c041a56ce43f4de1a873a4d1045261606e
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2025-06-20 22:37:37 +0100

    ecr and github actions permissions

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,16 @@
 output "rds_endpoint" {
   value = aws_db_instance.mysql.endpoint
 }
+
+# AWS Keys
+output "github_actions_access_key_id" {
+  value       = aws_iam_access_key.github_actions.id
+  description = "Access Key ID for GitHub Actions"
+  sensitive   = true
+}
+
+output "github_actions_secret_access_key" {
+  value       = aws_iam_access_key.github_actions.secret
+  description = "Secret Access Key for GitHub Actions"
+  sensitive   = true
+}
--- terraform/rds.tf
+++ terraform/rds.tf
@@ -20,7 +20,7 @@ resource "aws_db_instance" "mysql" {
   db_subnet_group_name    = aws_db_subnet_group.main.name
   vpc_security_group_ids  = [aws_security_group.rds_sg.id]
   backup_retention_period = 7
-  deletion_protection     = true
+  deletion_protection     = false
   multi_az                = false
   tags = {
     Name        = "savorpalette-mysql"
