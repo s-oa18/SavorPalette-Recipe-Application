0a0b8108f975b2c1d0681a420000a73bfc39f1df
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2025-06-20 10:45:26 +0100

    refactor db

--- database.php
+++ database.php
@@ -1,9 +1,9 @@
 <?php
 
-$host = "localhost";
-$dbname = "recipe_login_db";
-$username = "root";
-$password = "root";
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
 
 $mysqli = new mysqli($host, $username, $password, $dbname);
 
