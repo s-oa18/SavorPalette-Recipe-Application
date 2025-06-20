f90580973e260d8243e5557240b597bd7f0d0897
SethAmpofo <99514561+s-oa18@users.noreply.github.com>
2025-06-20 22:20:58 +0100

    rds connection changes to db and docker-compose file

--- database.php
+++ database.php
@@ -1,11 +1,11 @@
 <?php
 
-$host = getenv('DB_HOST');
-$dbname = getenv('DB_NAME');
-$username = getenv('DB_USER');
-$password = getenv('DB_PASSWORD');
+$servername = getenv('DB_HOST') ?: 'db';
+$username   = getenv('DB_USER') ?: 'root';
+$password   = getenv('DB_PASSWORD') ?: 'root';
+$dbname     = getenv('DB_NAME') ?: 'recipe_login_db';
 
-$mysqli = new mysqli($host, $username, $password, $dbname);
+$mysqli = new mysqli($servername, $username, $password, $dbname);
 
 if ($mysqli->connect_errno) {
     die("Connection error: " . $mysqli->connect_error);
